0b6655c617c5700c08fc748e82247050149867de
hoiae <85207194+hoiae@users.noreply.github.com>
2023-02-01 11:26:45 +0900

    [Silver II] Title: 최소 힙, Time: 360 ms, Memory: 26320 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,34 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+//최소힙
+public class Main {
+    static int n;
+    static int command;
+    static StringBuilder sb = new StringBuilder();
+    static PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            command = Integer.parseInt(br.readLine());
+            if (command == 0) {
+                Integer curr = pq.poll();
+                if (curr == null) {
+                    sb.append(0).append("\n");
+
+                } else {
+                    sb.append(curr).append("\n");
+                }
+                continue;
+            }
+            pq.add(command);
+        }
+        System.out.println(sb);
+    }
+}
