5e2f21c22ae714f25d244833d228d17798b0655b
hoiae <85207194+hoiae@users.noreply.github.com>
2023-05-23 16:29:03 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.05 ms, Memory: 71.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String solution(String number, int k) {
+        int n = number.length() - k;
+        int index = 0;
+        int max = 0;
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < n; i++){
+            max = 0;
+            for(int j = index; j <= number.length() - n + i; j++){
+                if(number.charAt(j) - '0' > max){
+                    max = (number.charAt(j) - '0');
+                    index = j + 1;
+                } 
+            }
+            sb.append(max);
+        }
+        return sb.toString();
+    }
+}
