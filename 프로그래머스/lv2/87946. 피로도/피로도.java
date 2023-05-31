7c98fd85ca5fca1389716690bc37826f5e478157
hoiae <85207194+hoiae@users.noreply.github.com>
2023-05-31 13:32:38 +0900

    [level 2] Title: 피로도, Time: 0.59 ms, Memory: 75.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/87946. 피로도/피로도.java
@@ -0,0 +1,34 @@
+import java.util.*;
+class Solution {
+    static boolean[] visited;
+    static int max = 0;
+    public int solution(int k, int[][] dungeons) {
+        visited = new boolean[dungeons.length];
+        for(int i = 0; i < dungeons.length; i++){
+            Arrays.fill(visited,false);
+            dfs(i,k,0,0,dungeons);
+        }
+        
+        
+        int answer = max;
+        return answer;
+    }
+    private static void dfs(int start,int k, int cnt, int depth, int[][] dungeons){
+        if(depth == dungeons.length){
+            max = Math.max(cnt,max);
+        }
+        for(int i = 0; i < dungeons.length; i++){
+            if(visited[i] == false){
+                visited[i] = true;
+                int dcnt = 0;
+                int dk = 0;
+                if(k >= dungeons[i][0]){
+                    dcnt = 1;
+                    dk = dungeons[i][1];
+                }
+                dfs(i, k - dk , cnt + dcnt, depth + 1, dungeons);
+                visited[i] = false;
+            }
+        }
+    }
+}
