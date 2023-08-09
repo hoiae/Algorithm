12db349a75aac050326cb842a330c57ca29e97cd
hoiae <85207194+hoiae@users.noreply.github.com>
2023-08-09 15:37:14 +0900

    [Platinum V] Title: 사탕상자, Time: 472 ms, Memory: 64696 KB -BaekjoonHub

--- 백준/Platinum/2243. 사탕상자/사탕상자.java
+++ 백준/Platinum/2243. 사탕상자/사탕상자.java
@@ -2,60 +2,71 @@
 import java.io.*;
 import java.util.StringTokenizer;
 
-//백준 2243 - 사탕상자
 public class Main {
-    static int n,a,b,c,firlstLeaf;
-    static int[] grade = new int[1000000 + 1];
-    static int[] tree;
+    static int MAX = 1000000;
+    static int leafnode;
+    static int[] tree = new int[MAX * 2];
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+//        tree세팅
+        leafnode = 1;
+        while(leafnode < MAX){
+            leafnode *= 2;
+        }
+
+        tree = new int[leafnode * 2];
+
+
         StringTokenizer st;
-
-        n = Integer.parseInt(br.readLine());
-        firlstLeaf = 1;
-        while(firlstLeaf < 1000000) firlstLeaf = firlstLeaf * 2;
-        tree = new int[firlstLeaf*2];
-
-        for(int i = 0;  i < n; i++){
+        for(int i = 0; i < n; i++){
             st = new StringTokenizer(br.readLine());
-            a = Integer.parseInt(st.nextToken());
-            //순위가 b인 사탕을 하나 꺼내어 출력함.
+            int a = Integer.parseInt(st.nextToken());
             if(a == 1){
-                b = Integer.parseInt(st.nextToken());
-                int candy = getCandy(1, firlstLeaf,1, b); //앞에서 부터 B번째에 있는 사탕 찾음
-                bw.write(candy+"\n");
-                update(candy,-1);
-            }
-            //우선순위가 b인 사탕을 양수개 만큼 넣거나 / 음수개 만큼 뺌;
-            else {
-                b = Integer.parseInt(st.nextToken());
-                c = Integer.parseInt(st.nextToken());
+                //사탕을 꺼내는 경우
+                //앞에서 부터 누적합을 사용해야함.
+                //getCandy(leafnode 시작인덱스,leafnode 마지막인덱스, 현재 node, )
+                //update해줌
+                int b = Integer.parseInt(st.nextToken());
+                int candy = getCandy(1, leafnode, 1, b);
+                update(candy, -1);
+                bw.write(candy + "\n");
+            }else{
+                //사탕을 넣는 경우
+                //update: tree[b]에서 c만큼 더해준 후, root 노드까지 갱신함.
+
+                int b = Integer.parseInt(st.nextToken());
+                int c = Integer.parseInt(st.nextToken());
                 update(b,c);
             }
         }
+
         bw.flush();
         bw.close();
+
     }
 
-    private static void update( int index,int diff) {
-        int start = firlstLeaf - 1 + index;
-        tree[start] += diff;
-        start /= 2;
-        while(start != 0){
-            tree[start] += diff;
-            start /= 2;
-        }
-    }
-
-    //이분탐색 세그먼트 트리
-    private static int getCandy(int left, int right, int node, int target) {
+    private static int getCandy(int left, int right, int node, int sum) {
         if(left == right) return left;
-        int mid  = (left + right)/2;
-        if(tree[node*2]>=target){
-            return getCandy(left,mid,node*2,target);
+        int mid = (left + right) / 2;
+        if(tree[node * 2] >= sum){
+            return getCandy(left, mid, node * 2, sum);
         }else{
-            return getCandy(mid + 1, right,node*2+1,target-tree[node*2]);
+            return getCandy(mid + 1, right, node*2 + 1, sum - tree[node * 2]);
+        }
+
+    }
+
+    private static void update(int index, int diff) {
+        index = leafnode + index - 1;
+        tree[index] += diff;
+        
+        while( index > 1){
+            tree[index / 2] += diff;
+            index = index / 2;
         }
     }
 }
