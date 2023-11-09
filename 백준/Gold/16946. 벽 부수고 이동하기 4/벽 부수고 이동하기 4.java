3faf7eafcbc1c79f9d2e119912ae0299eecf35f2
hoiae <85207194+hoiae@users.noreply.github.com>
2023-11-09 14:56:34 +0900

    [Gold II] Title: 벽 부수고 이동하기 4, Time: 740 ms, Memory: 196728 KB -BaekjoonHub

--- 백준/Gold/16946. 벽 부수고 이동하기 4/벽 부수고 이동하기 4.java
+++ 백준/Gold/16946. 벽 부수고 이동하기 4/벽 부수고 이동하기 4.java
@@ -1,124 +1,126 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
-import java.util.Map;
 import java.util.Queue;
-import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int[][] arr,group;
 	static int n;
 	static int m;
-	static Map<Integer,Integer> groupSize;
-	public static void main(String[] args) throws Exception{
-
-		//벽 부수고 이동하기 4 BFS/분리집합
+	static char map[][];
+	static int dx[] = {0,0,1,-1};
+	static int dy[] = {-1,1,0,0};
+	static int group[][];
+	static HashMap<Integer, Integer> hm = new HashMap<>();
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		st = new StringTokenizer(br.readLine());
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken()); // 행 길이
+		m = Integer.parseInt(st.nextToken()); // 열 길이
+		map = new char[n][m]; // 지도 표시
+		group = new int[n][m]; // 0 그룹화 map
+		int index = 1; // 그룹번호
 		
-		arr = new int[n][m];
-		group = new int[n][m];
-		for(int i=0;i<n;i++) {
-			String[] str = br.readLine().split("");
-			for(int j=0;j<m;j++){
-				arr[i][j]= Integer.parseInt(str[j]);
-			}
+		//map 입력
+		for(int i = 0; i < n; i++) {
+			map[i] = br.readLine().toCharArray();
 		}
-		
-		//각각 그룹마다 다른 key값을 가지게해줌 
-		int groupCnt=1;
-		groupSize = new HashMap<>(); 
-		for(int i=0;i<n;i++) {
-			for(int j=0;j<m;j++) {
-				if(arr[i][j]==0 && group[i][j]==0) {
-					groupSize.put(groupCnt, bfs(i,j,groupCnt));
-					groupCnt++;
-				}
-			}
-		}
-		//답을 이중배열로 출력함
-		StringBuilder sb = new StringBuilder();
+	
+		// 0 그룹화 하기 => 그룹 번호 부여, 같은 그룹 개수 세기
 		for(int i = 0; i < n; i++) {
 			for(int j = 0; j < m; j++) {
-				if(group[i][j]==0) {
-					sb.append(count(i,j)+"");
-					continue;
+				if(map[i][j] == '0' && group[i][j] == 0){
+					hm.put(index, bfs(i, j, index));
+					index++;
 				}
-					sb.append(0+"");
+			}
+		}
+		
+		StringBuffer sb = new StringBuffer();
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < m; j++) {
+				sb.append(mapCount(i,j));
 			}
 			sb.append("\n");
 		}
-		System.out.println(sb);
 		
+		System.out.println(sb.toString());
 	}
-	//벽에 맞닿은 그룹의 합을 구함
-	static int count(int x,int y) {
-		int cnt=1;
-		if(arr[x][y]==0) return 0;
-		Set<Integer> set = new HashSet<>();
+	
+	static int mapCount(int X, int Y) {
+		int sum = 1;
+		HashSet<Integer> hs = new HashSet<>();
 		
-		//벽에 맞닿은 4방향만 구하면됨
-		//그 방향의 그룹의 0의 갯수 정보는 이미 구했기 때문
-		for(int i=0;i<4;i++) {
-			int[] dx = {0,1,0,-1};
-			int[] dy = {1,0,-1,0};
+		if(map[X][Y] == '0') {
+			return 0;
+		}
+		
+		for(int i = 0; i < 4; i++) {
+			int x = X + dx[i];
+			int y = Y + dy[i];
 			
-			int sx = x+dx[i];
-			int sy = y+dy[i];
-			
-			if(sx < 0 || sy < 0 || sx>=n || sy >= m || group[sx][sy]==0)
+			if(x < 0 || n <= x || y < 0 || m <= y) { //범위 초과시 무시
 				continue;
-			//맞닿은 그룹이 중복일 경우를 위해 set에 저장함
-			set.add(group[sx][sy]);
+			}
 			
-		}
-		for(int size : set) {
-			cnt+=groupSize.get(size);
+			if(group[x][y] == 0) {
+				continue;
+			}
+			
+			if(map[x][y] == '0') { // 벽이 아닐때
+				hs.add(group[x][y]);
+			}
+
 		}
 		
-		return cnt%10;
+		for(int ind: hs) {
+			sum += hm.get(ind);
+		}
+		
+		return sum%10;
+		
 	}
-	//그룹마다 몇개의 0이 포함되었는지를 리턴해줌
-	static int bfs(int x,int y,int groupCnt) {
-		int cnt=1;
-		Queue<Point> q = new LinkedList<>();
-		q.add(new Point(x,y));
-		group[x][y]=groupCnt;
+	
+	
+	static int bfs(int s, int e, int groupNum) {
+		int count = 1;
+		Queue<coordinate> q = new LinkedList<>();
+		q.offer(new coordinate(s, e));
+		group[s][e] = groupNum;
+		
 		while(!q.isEmpty()) {
-			Point point = q.poll(); 
-			int[] dx = {0,1,0,-1};
-			int[] dy = {1,0,-1,0};
-			for(int i=0;i<4;i++) {
-				int sx = point.x+dx[i];
-				int sy = point.y+dy[i];
+			coordinate cur = q.poll();
+
+			for(int i = 0; i < 4; i++) {
+				int x = cur.x + dx[i];
+				int y = cur.y + dy[i];
 				
-				if(sx < 0 || sy <0 || sx >= n || sy >= m)
+				if(x < 0 || n <= x || y < 0 || m <= y) { //범위 초과시 무시
 					continue;
-				//아직 그룹에 속하지 않았고 && 벽이 아니라면 카운트해준다.
-				if(group[sx][sy]==0 && arr[sx][sy]==0) {
-					group[sx][sy]=groupCnt;
-					cnt++;
-					q.add(new Point(sx,sy));
 				}
+				
+				if(map[x][y] == '0' && group[x][y] == 0) {
+					q.offer(new coordinate(x,y));
+					group[x][y] = groupNum;
+					count++;
+				}
+				
 			}
 		}
-		return cnt;
-	
+
+		return count;
 	}
 
-}
-class Point{
-	int x;
-	int y;
+	static class coordinate {
+		int x;
+		int y;
 	
-	Point(int x, int y){
-		this.x = x;
-		this.y = y;
+		coordinate(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
 	}
 }
