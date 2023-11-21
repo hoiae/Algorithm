591bcc70a1f1d4ee2fbcbdf4e2fe36bd11927d09
hoiae <85207194+hoiae@users.noreply.github.com>
2023-11-21 16:39:47 +0900

    [Gold III] Title: 앱, Time: 292 ms, Memory: 22648 KB -BaekjoonHub

--- 백준/Gold/7579. 앱/앱.java
+++ 백준/Gold/7579. 앱/앱.java
@@ -1,53 +1,36 @@
-
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
-//백준 7579번 - 앱
 public class Main {
-    static int N, M, totalCost;
-    static int[] m;
-    static int[] c;
-    static int[][] dp;
-    static boolean[] visit;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
-        m = new int[N + 1];
-        c = new int[N + 1];
-        visit = new boolean[N + 1];
-        StringTokenizer st1 = new StringTokenizer(br.readLine());
-        StringTokenizer st2 = new StringTokenizer(br.readLine());
-        for (int i = 1; i <= N; i++) {
-            m[i] = Integer.parseInt(st1.nextToken());//메모리
-            c[i] = Integer.parseInt(st2.nextToken());//소모비용
-            totalCost += c[i];
-        }
+        int[] memories = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int[] costs = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
 
-        dp = new int[N + 1][totalCost + 1];// i번 앱까지 중, j 만큼의 비용을 사용하여 만들수 있는 가장 많은 메모리
-        for (int i = 1; i <= N; i++) {
-            for (int j = 1; j <= totalCost; j++) {
-                dp[i][j] = Math.max(dp[i][j],dp[i-1][j]);//기초값 이게뭐노
-                if(j - c[i] >= 0){
-                    dp[i][j] = Math.max(dp[i][j],dp[i - 1][j - c[i]] + m[i]); //이전값 + 현재 확보한 메모리
+        int ans = Integer.MAX_VALUE;
+        int[][] dp = new int[n + 1][10000 + 1];
+        for(int i = 1;  i <= n; i++){
+            int memory = memories[i - 1];
+            int cost = costs[i - 1];
+            for (int j = 0; j <= 10000; j++) {
+                if(j >= cost){
+                    dp[i][j] = Math.max(dp[i - 1][j - cost] + memory, dp[i - 1][j]);
+                }else{
+                    dp[i][j] = dp[i - 1][j];
+                }
+                if(dp[i][j] >= m){
+                    ans = Math.min(j, ans);
                 }
             }
         }
-        int result = 0;
-        for(int i = 1; i <= totalCost; i++){
-            if(dp[N][i] >= M){
-                result = i;
-                break;
-            }
-        }
-        bw.write(result+"\n");
-        bw.flush();
-        bw.close();
-
+        System.out.println(ans);
     }
 }
