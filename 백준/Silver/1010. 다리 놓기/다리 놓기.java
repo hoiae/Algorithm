776f8445d049d83102b70bfcf40ad0365246928e
hoiae <85207194+hoiae@users.noreply.github.com>
2024-02-28 12:07:25 +0900

    [Silver V] Title: 다리 놓기, Time: 84 ms, Memory: 12432 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.java
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -1,33 +1,40 @@
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
+import java.io.*;
+import java.util.*;
 public class Main {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    public static void main(String[] args) throws IOException {
-        int n = Integer.parseInt(br.readLine());
+	static BufferedReader br;
+	static int N,M;
+	static boolean[] visited;
+	static int cnt;
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int t = Integer.parseInt(br.readLine());
+		
+		for(int i = 0; i < t; i++) {
+			sb.append(solve()).append("\n");
+		}
+		
+		System.out.println(sb);
+	}
 
-        for (int i = 0; i < n; i++) {
-            solve();
-        }
-    }
+	private static int solve() throws IOException {
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		int[][] dp = new int[M+1][M+1];
+		
+		dp[0][0] = 1;
+		for(int i = 1; i <= M; i++) {
+			dp[i][0] = 1;
+			for(int j = 1; j <= i; j++) {
+				dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+			}
+		}
+		
+		
+		return dp[M][N];
+	}
 
-    private static void solve() throws IOException {
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-
-        int[][] dp = new int[M + 1][M + 1];
-            dp[0][0] = 1;
-        for(int i = 0;  i <= M; i++){
-            dp[i][0] = 1;
-            for(int j = 1; j <= i; j++){
-                dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
-            }
-        }
-        System.out.println(dp[M][N]);
-    }
 }
