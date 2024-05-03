c4448b823a11e4a0b51b55c62c7174631bcdb2b2
hoiae <85207194+hoiae@users.noreply.github.com>
2024-05-03 09:29:35 +0900

    [level 4] Title: 올바른 괄호의 갯수, Time: 4299.90 ms, Memory: 383 MB -BaekjoonHub

---
+++ 프로그래머스/4/12929. 올바른 괄호의 갯수/올바른 괄호의 갯수.java
@@ -0,0 +1,47 @@
+import java.util.*;
+class Solution {
+    static int n;
+    static int cnt;
+    public int solution(int n) {
+        //)는 앞에 있는 (의 개수를 초과할 수 없다.
+        this.n = n;
+        
+        //깊이, 사용한 '('의 개수
+        dfs(0,0,"");
+        return cnt;
+    }
+    private static void dfs(int depth, int left, String str){
+        if(depth == n * 2){
+            //유효한지 판단
+            if(left <= n){
+                if(checkValid(str)){
+                    cnt++;
+                };   
+            }
+            return;
+        }
+        
+        //+(
+        if(left < n){    
+        dfs(depth + 1, left + 1, str+"(");
+        }
+        //+)
+        if(depth - left < n){
+        dfs(depth + 1, left, str+")");
+        }
+    }
+    private static boolean checkValid(String str){
+        Stack<Character> stack = new Stack<>();
+        for(int i = 0; i < str.length(); i++){
+            char c = str.charAt(i);
+            if(c == '('){
+                stack.push(c);
+            }else{
+                if(stack.isEmpty() || stack.peek() == ')') return false;
+                stack.pop();
+            }
+        }
+        if(!stack.isEmpty()) return false;
+        return true;
+    }
+}
