ca3364a748dcd0b75836103b12c2615930d8a41d
hoiae <85207194+hoiae@users.noreply.github.com>
2024-07-01 18:11:34 +0900

    [Gold V] Title: 평범한 배낭, Time: 204 ms, Memory: 53704 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,53 +1,37 @@
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int[] weight;
-    static int[] value;
-    static int[][] dp;
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
-
-        weight = new int[n + 1];
-        value = new int[n + 1];
-        dp = new int[k + 1][n + 1];
-
-        //n만큼 배열 초기화
+        
+        //무게,가치
+        int[] weights = new int[n + 1];
+        int[] values = new int[n + 1]; 
+        
         for(int i = 1; i <= n; i++){
             st = new StringTokenizer(br.readLine());
-            int w = Integer.parseInt(st.nextToken());
-            int v = Integer.parseInt(st.nextToken());
-            weight[i] = w;
-            value[i] = v;
+            weights[i] = Integer.parseInt(st.nextToken());
+            values[i] = Integer.parseInt(st.nextToken());    
         }
-
-        //dp[i][j]를 채움
-        for(int i = 1; i <= k; i++){
-            for(int j = 1; j <= n; j++){
-                //i = 현재 무게
-                //j = 무게와 벨류
-                //현재 무게 보다 j번째 무게가 더 큰 경우(가방에 담을 수 없는 무게인 경우)
-                if(weight[j] >  i){
-                    dp[i][j] = dp[i][j - 1];
-                }else{ //가방에 담을 수 있는 경우
-                    int spareWeight = i - weight[j];
-                    int spareValue = 0;
-                    for(int s = 1; s < j; s++){
-                        spareValue = Math.max(dp[spareWeight][s], spareValue);
-                    }
-                    dp[i][j] = Math.max(dp[i][j - 1], value[j] + spareValue);
+        
+        //dp
+        int[][] dp = new int[n + 1][k + 1];
+        for(int i = 1; i <= k; i++){//제한무게
+            for(int j = 1; j <= n; j++){//상품인덱스
+                if(weights[j] > i){
+                    dp[j][i] = dp[j - 1][i];
+                    continue;
                 }
+                
+                //가방에 담을 수 있는 경우
+                dp[j][i] = Math.max(dp[j - 1][i], values[j] + dp[j - 1][i - weights[j]]);
             }
         }
-
-        System.out.println(dp[k][n]);
-
-
+        System.out.println(dp[n][k]);
     }
 }
