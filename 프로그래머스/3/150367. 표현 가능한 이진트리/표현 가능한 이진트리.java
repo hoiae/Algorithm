0eec56c5f026ebf55d9d28196eaa40784e1258fe
hoiae <85207194+hoiae@users.noreply.github.com>
2024-07-02 23:21:59 +0900

    [level 3] Title: 표현 가능한 이진트리, Time: 18.13 ms, Memory: 92.6 MB -BaekjoonHub

--- 프로그래머스/3/150367. 표현 가능한 이진트리/표현 가능한 이진트리.java
+++ 프로그래머스/3/150367. 표현 가능한 이진트리/표현 가능한 이진트리.java
@@ -1,41 +1,65 @@
-public class Solution {
-	private static int[] answer;
-	private static boolean dfs(String number) {
-		boolean valid = true;
-		
-		int mid = (number.length()-1)/2;
-		char root = number.charAt(mid);
-		String left = number.substring(0,mid);
-		String right = number.substring(mid+1,number.length());
-		
-		if(root == '0' && (left.charAt((left.length()-1)/2)=='1' || right.charAt((right.length()-1)/2)=='1')){
-			return false;
-		}
-		
-		if(left.length() >= 3) {
-			valid = dfs(left);
-			if(valid) {
-				valid = dfs(right);
-			}
-		}
-		return valid;
-	}
+import java.io.*;
+import java.util.*;
+class Solution {
     public int[] solution(long[] numbers) {
-        answer = new int [numbers.length];
-        
-        for(int i = 0; i < numbers.length; i++) {
-        	String cur = Long.toBinaryString(numbers[i]);
-        	int j = 0;
-        	while((int)Math.pow(2, j)-1 < cur.length()) {
-        		j++;
-        	}
-        	while((int)Math.pow(2, j)-1 != cur.length()) {
-        		cur = "0"+ cur;
-        	}
-        	if(dfs(cur)) {
-        		answer[i] = 1;
-        	}
+        int[] ans = new int[numbers.length];
+        for(int i = 0; i < numbers.length; i++){
+            String binary = Long.toBinaryString(numbers[i]);
+            //포화 이진트리 노드의 개수는  2^h - 1 //h는 높이
+            int size = 1;
+            while(size - 1 < binary.length()){
+                size = size << 1;
+              
+            }
+            size--;
+            //size와의 차이 만큼 앞에 0을 붙임
+            StringBuilder sb = new StringBuilder(binary);
+            for(int j = 0; j < size - binary.length(); j++){
+                sb.insert(0,"0");
+            }
+            
+            //dfs하며 유효한지 판단함.
+            if(dfs(sb.toString())){
+                ans[i] = 1;
+            }
         }
-        return answer;
+        
+   
+        
+//         for(int i = 0; i < numbers.length; i++) {
+//         	String cur = Long.toBinaryString(numbers[i]);
+//         	int j = 0;
+//         	while((int)Math.pow(2, j)-1 < cur.length()) {
+//         		j++;
+//         	}
+//         	while((int)Math.pow(2, j)-1 != cur.length()) {
+//         		cur = "0"+ cur;
+//         	}
+//         	if(dfs(cur)) {
+//         		ans[i] = 1;
+//         	}
+//         }
+  
+        //0111111
+        return ans;
+    }
+    private static boolean dfs(String binary){
+        int mid = (binary.length() - 1) / 2;
+        char root = binary.charAt(mid);
+        String left = binary.substring(0, mid);
+        String right = binary.substring(mid + 1, binary.length());
+        
+        if(root == '0' &&
+          (left.charAt((left.length() - 1)/2) == '1' || right.charAt((right.length() - 1) / 2) == '1')){
+            return false;
+        }
+      
+        
+        if(left.length() >= 3){
+            if(!dfs(left) || !dfs(right)){
+                return false;
+            }
+        }
+        return true;
     }
 }
