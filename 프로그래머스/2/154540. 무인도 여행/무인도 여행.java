b4ad01ea1562a62a6b3311e78534666d545db3f2
hoiae <85207194+hoiae@users.noreply.github.com>
2024-07-24 09:27:00 +0900

    [level 2] Title: 무인도 여행, Time: 8.65 ms, Memory: 81.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/154540. 무인도 여행/무인도 여행.java
@@ -0,0 +1,59 @@
+import java.util.*;
+class Solution {
+    static boolean[][] visited;
+    static int n,m;
+    static int[] dx = {-1,1,0,0};
+    static int[] dy = {0,0,-1,1};
+    static String[] maps;
+    public int[] solution(String[] maps) {
+        n = maps.length;
+        m = maps[0].length();
+        this.maps = maps;
+        
+        visited = new boolean[n][m];
+        List<Integer> days = new ArrayList<>();
+        for(int i = 0;  i < n; i++){
+            for(int j = 0;  j < m; j++){
+                if(visited[i][j] == true || maps[i].charAt(j)=='X'){
+                    continue;
+                }
+                days.add(bfs(i,j));
+                
+            }
+        }
+        
+        // days.sort((o1,o2) -> {return o1-o2;});
+        Collections.sort(days);
+        if(days.size() == 0){
+            return new int[]{-1};
+        }
+        
+        int[] answer = new int[days.size()];
+        for(int i = 0; i < days.size(); i++){
+            answer[i] = days.get(i);
+        }
+
+        return answer;
+    }
+    private static int bfs(int x, int y){
+        visited[x][y] = true;
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{x,y});
+        
+        int sum = 0;
+        while(!q.isEmpty()){
+            int[] now = q.poll();
+            sum +=  maps[now[0]].charAt(now[1]) - '0';
+            for(int i = 0; i < 4; i++){
+                int nx = now[0] + dx[i];
+                int ny = now[1] + dy[i];
+                
+                if(nx < 0 || nx >= n || ny < 0 || ny >= m || visited[nx][ny] || maps[nx].charAt(ny)=='X')
+                    continue;
+                visited[nx][ny] = true;
+                q.add(new int[]{nx,ny});
+            }
+        }
+        return sum;
+    }
+}
