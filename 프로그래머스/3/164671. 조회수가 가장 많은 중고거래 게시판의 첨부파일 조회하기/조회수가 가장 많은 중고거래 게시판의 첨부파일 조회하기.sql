be0a59330f53271a4b3a65b368a5f9f04ad68efb
hoiae <85207194+hoiae@users.noreply.github.com>
2024-08-05 14:40:46 +0900

    [level 3] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
+++ 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -4,30 +4,16 @@
 --     CAT('/home/grep/src/',A.BOARD_ID,A.FILE_ID,FILE_NAME,'.',FILE_EXT) FILE_PATH
  
 
-# SELECT
-#      CONCAT('/home/grep/src/',B.BOARD_ID,"/", A.FILE_ID, A.FILE_NAME, A.FILE_EXT) FILE_PATH
-#      FROM USED_GOODS_FILE A
-#      JOIN 
-#          (SELECT
-#             BOARD_ID,
-#             MAX(VIEWS) VIEWS
-#             FROM USED_GOODS_BOARD) B
-#         ON A.BOARD_ID = B.BOARD_ID
-#     ORDER BY A.FILE_ID DESC;
+SELECT
+     CONCAT('/home/grep/src/',B.BOARD_ID,"/", A.FILE_ID, A.FILE_NAME, A.FILE_EXT) FILE_PATH
+     FROM USED_GOODS_FILE A
+     JOIN 
+         (SELECT
+            BOARD_ID,
+            VIEWS
+            FROM USED_GOODS_BOARD
+            ORDER BY VIEWS DESC
+            LIMIT 1) B
+        ON A.BOARD_ID = B.BOARD_ID
+    ORDER BY A.FILE_ID DESC;
 
--- 코드를 입력하세요
-WITH TOP_BOARD AS(
-SELECT  BOARD_ID
-  FROM  USED_GOODS_BOARD
- ORDER
-    BY  VIEWS DESC
- LIMIT  1)
-
-SELECT  CONCAT('/home/grep/src/',BOARD_ID,'/',FILE_ID,FILE_NAME,FILE_EXT) as FILE_PATH
-  FROM  USED_GOODS_FILE AS UGF
- WHERE  UGF.BOARD_ID =(
-         SELECT  BOARD_ID
-           FROM  TOP_BOARD)
- ORDER
-    BY  FILE_ID DESC
-;
