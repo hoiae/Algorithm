7cbb7a6a489df08ed20ea4a96e0881fb09665d59
hoiae <85207194+hoiae@users.noreply.github.com>
2024-10-12 00:46:37 +0900

    [Gold IV] Title: 문자열 폭발, Time: 624 ms, Memory: 70852 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
@@ -1,55 +1,50 @@
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Stack;
-
+import java.util.*;
+import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String input = br.readLine();
-        String bomb = br.readLine();
-
-        int bombLastIndex = bomb.length() - 1;
-        char lastWord = bomb.charAt(bombLastIndex);
-
-        Stack<Character> stack = new Stack<>();
-        //stack에 하나씩 담음.
-        //lastWord와 동일한 경우, stack에서 하나씩 꺼내서 비교함
-        //같은 경우 삭제
-        //다른 경우 다시 추가함.
-        for(int i = 0;  i < input.length(); i++){
-            stack.push(input.charAt(i));
-            if(stack.peek() == lastWord){
+        String boom = br.readLine();
+        
+        //폭발 문자열은 같은 문자를 두 개 이상 포함하지 않는다.
+        //stack에 문자열을 담는다. 
+        //만약 boom.length보다 stack이크고, stack.peek()가 boom의 마지막 문자열과 같으면 하나씩 꺼낸다.
+        //boom과 동일하면 버린다.
+        //boom과 다르면 다시 넣는다.
+        int n = boom.length();
+        Stack<Character> stack = new Stack();
+        for(char c : input.toCharArray()){
+            stack.push(c);
+            if(stack.peek() == boom.charAt(n - 1) && stack.size() >= n){
                 Stack<Character> temp = new Stack<>();
-                temp.push(stack.pop());
-                for(int j = bombLastIndex - 1; j >=0; j--){
-                    if(!stack.isEmpty() && bomb.charAt(j) == stack.peek()){
+                //뒤에서 부터 하나씩 거꾸로 비교한다.
+                for(int j = 0; j < n; j++){
+                    if(stack.peek() == boom.charAt(n - 1 - j)){
                         temp.push(stack.pop());
-                    }else{ //다시 기존 스택에 옮겨 담음
-                        while(!temp.isEmpty()){
-                            stack.push(temp.pop());
-                        }
-                        break;
+                        continue;
+                    }
+                    break;
+                }
+                //stack이 폭발문자열이 아닌 경우
+                if(temp.size() != n){
+                    while(!temp.isEmpty()){
+                        stack.push(temp.pop());
                     }
                 }
+                
             }
         }
-
-        int stackSize = stack.size();
-        char[] reverse = new char[stackSize];
-        for(int i = 0;  i <stackSize; i++){
-            char now = stack.pop();
-            reverse[i] = now;
-        }
-        StringBuilder sb = new StringBuilder();
-       for(int i = reverse.length - 1; i >= 0; i--){
-           sb.append(reverse[i]);
-       }
-       String ans = sb.toString();
-       if(ans.length() == 0){
-           ans = "FRULA";
-       }
-       System.out.println(ans);
+        
+        if(stack.size() > 0){
+            StringBuilder sb = new StringBuilder();
+            for(int i = 0;  i < stack.size(); i++){
+                sb.append(stack.get(i));
+            }
+            System.out.println(sb.toString());
+        }else{
+            System.out.println("FRULA");
+        } 
+    
+                
     }
 }
