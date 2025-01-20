1166061eedd68b5398ed96e89fb24cdc592aff80
hoiae <85207194+hoiae@users.noreply.github.com>
2025-01-20 15:39:19 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지, Time: 54.71 ms, Memory: 119 MB -BaekjoonHub

--- 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/README.md
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/README.md
@@ -1,10 +1,10 @@
 # [level 2] [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지 - 340212 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/340212?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/340212) 
 
 ### 성능 요약
 
-메모리: 121 MB, 시간: 36.79 ms
+메모리: 119 MB, 시간: 54.71 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 20일 15:34:38
+2025년 01월 20일 15:39:17
 
 ### 문제 설명
 
--- 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
@@ -1,35 +1,54 @@
 class Solution {
     public int solution(int[] diffs, int[] times, long limit) {
-        int left = 1; // 숙련도는 최소 1부터 시작
-        int right = 100_000; // diffs[i]의 최대값
-        int answer = right;
-
-        while (left < right) { // left == right가 될 때 종료
-            int mid = (left + right) / 2;
-            if (solve(diffs, times, mid, limit)) {
-                answer = mid; // 가능한 경우 정답 갱신
-                right = mid; // 더 낮은 숙련도를 탐색
-            } else {
-                left = mid + 1; // 더 높은 숙련도를 탐색
+        // 틀린경우,
+        // time_cur 만큼의 시간을 사용한다.
+        // time_prev만큼의 시간을 소모하여, 이전 퍼즐을 다시 풀고와야한다.
+        
+        // 퍼즐게임에는 전체 제한 시간 limit이 정해져 있다.
+        // 제한 시간내에 퍼즐을 모두 해결하기 위해서 숙련도의 "최솟값"을 구해야한다.
+        int answer = Integer.MAX_VALUE;;
+        
+        //diffs를 기준으로한다.
+        int left = 1;
+        int right = 100_000;
+        
+        //이분탐색
+        while(left <= right){
+            int level = (left + right) / 2;
+            
+            long result = solve(diffs, times, level,limit);
+            if(result == -1){
+                //-1인경우는 limit을 초과한 것이므로 level을 더 높여야한다.
+                left = level + 1;
+            }else{
+                //정상적으로 동작한 경우, level을더 낮춘다.
+                answer = Math.min(answer, level);
+                right = level - 1;
             }
+ 
         }
         return answer;
     }
-
-    private boolean solve(int[] diffs, int[] times, int level, long limit) {
-        long spend = times[0]; // 첫 번째 퍼즐 시간
-
-        for (int i = 1; i < diffs.length; i++) {
-            if (diffs[i] <= level) {
+    private long solve(int[] diffs, int[] times, int level, long limit){
+        long spend = 0;
+        
+        //순차적으로 진행한경우
+        for(int i = 0 ; i < diffs.length; i++){            
+            //level을 parameter를 인자로 받아서 로직 진행
+            if(diffs[i] <= level){
                 spend += times[i];
-            } else {
-                int count = diffs[i] - level; // 틀리는 횟수
-                spend += (long) count * (times[i - 1] + times[i]); // 반복 시간
-                spend += times[i]; // 마지막으로 문제를 푼 시간
+            }else{
+                int count = diffs[i] - level; //이전 문제 + 현재 문제를 다시 풀어야하는 횟수;
+                spend += count * (times[i - 1] + times[i]);//반복되는 시간
+                spend += times[i];//현재 문제를 정상적으로 푼다.
             }
-
-            if (spend > limit) return false; // 제한 시간 초과
         }
-        return true;
+        
+        //limit을 초과한 경우 종료시킨다.
+        if(spend > limit){
+            return -1; // -1를 return한 경우 인자인 level을 높여야한다. 낮추면 또 실패하기 때문
+        }
+        
+        return spend; //정상적으로 return된 경우 이므로 인자인 leve을 낮춘다.
     }
 }
